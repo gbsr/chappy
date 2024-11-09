46da778608944a9219d60f367620fe67f1178d51
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-11-09 18:39:19 +0100

    another try fixing render deploy then

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 1338,
+    host: true
+  }
 });
